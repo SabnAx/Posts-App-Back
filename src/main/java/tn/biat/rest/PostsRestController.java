4d64ba94e8d9b7b6e4acfb51d1491f0188b945b9
unknown <sami.benachour@gmail.com>
2018-05-31 11:30:44 +0100

    GET paramétré

--- src/main/java/tn/biat/rest/PostsRestController.java
+++ src/main/java/tn/biat/rest/PostsRestController.java
@@ -4,15 +4,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import tn.biat.domain.Post;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PostsRestController {
 
 	private static Map<Integer, Post> data;
@@ -28,5 +32,15 @@ public class PostsRestController {
 	@GetMapping(path = "/api/posts")
 	public List<Post> findAll() {
 		return new ArrayList<Post>(data.values());
+	}
+
+	@GetMapping(path = "/api/posts/{id}")
+	public ResponseEntity<Post> findById(@PathVariable int id) {
+		Optional<Post> resultat = data.values().stream().filter(p -> p.getId() == id).findFirst();
+		if (resultat.isPresent()) {
+			return new ResponseEntity<Post>(resultat.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 }
